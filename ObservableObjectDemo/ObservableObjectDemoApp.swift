b58f74b120d6bbb9a1323e61845578814cb0529c
Davide Troise <davide@troise.net>
2022-12-27 19:09:39 +0100

    Initial Commit

---
+++ ObservableObjectDemo/ObservableObjectDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  ObservableObjectDemoApp.swift
+//  ObservableObjectDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct ObservableObjectDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
