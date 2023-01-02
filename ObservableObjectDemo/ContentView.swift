db4b7d4cdc85774bfc744362603a90d8e930bb77
Davide Troise <davide@troise.net>
2023-01-02 01:32:31 +0100

    Complete code

--- ObservableObjectDemo/ContentView.swift
+++ ObservableObjectDemo/ContentView.swift
@@ -8,13 +8,39 @@
 
 import SwiftUI
 
+class Contact: ObservableObject {
+    var name: String
+    @Published var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+    func haveBirthday() {
+        age += 1
+    }
+}
+
 struct ContentView: View {
+    @ObservedObject private var contact = Contact(name: "Davide", age: 41)
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button {
+                contact.haveBirthday()
+            } label: {
+                Text("Have birthday")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(.red, in: RoundedRectangle(cornerRadius: 12.0))
+            }
+
+            Text("\(contact.name) is \(contact.age) years old")
+                .font(.title2)
+                .foregroundColor(.secondary)
+                .padding()
         }
         .padding()
     }
